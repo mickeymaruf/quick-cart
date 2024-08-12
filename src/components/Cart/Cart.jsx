b20d65c18b7af973495bfdf1839a0dee51ade994
mickeymaruf <mickeymaruf@gmail.com>
2024-08-12 19:01:56 +0600

    Updated watermark logo

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,6 +6,7 @@ import Button from "../Button";
 import { CgChevronLeft } from "react-icons/cg";
 import { HiOutlineShoppingCart } from "react-icons/hi2";
 import { IoCloseOutline } from "react-icons/io5";
+import { FiShoppingCart } from "react-icons/fi";
 
 export function Cart() {
   const { cart, opened, setOpened, apiKey, APP_API_URL } = useCart();
@@ -61,7 +62,7 @@ export function Cart() {
         <div className="flex h-full flex-col justify-between">
           <div className="overflow-y-auto">
             {cart.length === 0 ? (
-              <div className="relative flex h-screen flex-col items-center justify-center text-center font-semibold">
+              <div className="relative flex h-screen flex-col items-center justify-center text-center">
                 <div className="absolute left-0 top-0 flex w-full items-center justify-between p-5 md:p-8">
                   <h5 className="text-xl font-medium">My Cart</h5>
                   <button
@@ -73,15 +74,18 @@ export function Cart() {
                   </button>
                 </div>
 
-                <HiOutlineShoppingCart size={50} />
-                <p className="mt-1 text-2xl">Your cart is empty.</p>
+                <FiShoppingCart size={50} />
+                <p className="mt-1 text-2xl font-semibold">
+                  Your cart is empty.
+                </p>
                 <div className="absolute bottom-4 left-1/2 flex -translate-x-1/2 items-center justify-center gap-x-2">
                   <span className="text-sm">Powered by</span>
                   <a href="https://quickkcart.vercel.app/" target="_blank">
-                    <div className="relative flex w-fit items-center gap-2 text-lg font-bold">
-                      <FaOpencart className="text-blue-600" />
-                      Quick Cart
-                      <div className="absolute -bottom-0 right-0 h-0.5 w-5 bg-gray-300"></div>
+                    <div className="flex w-fit items-center gap-1.5">
+                      <div className="rounded bg-blue-500 p-1 pr-1.5 text-white">
+                        <FiShoppingCart size={16} />
+                      </div>
+                      <h5 className="font-bold">quick-cart</h5>
                     </div>
                   </a>
                 </div>
@@ -142,10 +146,11 @@ export function Cart() {
               <div className="mt-4 flex items-center justify-center gap-x-2">
                 <span className="text-sm text-gray-500">Powered by</span>
                 <a href="https://quickkcart.vercel.app/" target="_blank">
-                  <div className="relative flex w-fit items-center gap-2 whitespace-nowrap text-lg font-bold">
-                    <FaOpencart className="text-blue-600" />
-                    Quick Cart
-                    <div className="absolute -bottom-0 right-0 h-0.5 w-5 bg-gray-300"></div>
+                  <div className="flex w-fit items-center gap-1.5">
+                    <div className="rounded bg-blue-500 p-1 pr-1.5 text-white">
+                      <FiShoppingCart size={16} />
+                    </div>
+                    <h5 className="font-bold">quick-cart</h5>
                   </div>
                 </a>
               </div>
@@ -160,7 +165,7 @@ export function Cart() {
         onClick={() => setOpened((s) => !s)}
         className="fixed bottom-8 right-8 rounded-lg border bg-gray-100 p-3.5"
       >
-        <HiOutlineShoppingCart size={20} />
+        <FiShoppingCart size={20} />
         <div className="absolute -right-3 -top-1 flex h-5 w-5 items-center justify-center rounded-md bg-blue-600 text-xs font-medium text-white">
           {totalProducts}
         </div>
